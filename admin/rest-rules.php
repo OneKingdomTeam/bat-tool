5a7572f0251ed269a1155c304384e296f7e5c74b
VrubPetGit <petr@vrubel.online>
2023-08-23 11:10:45 +0200

    work on data storing

--- admin/rest-rules.php
+++ admin/rest-rules.php
@@ -12,6 +12,29 @@ add_filter( 'rest_authentication_errors', function( $result ) {
         return $result;
     }
 
+    // No authentication has been performed yet.
+    // Return an error if user is not logged in.
+    if ( ! is_user_logged_in() ) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            __( 'You are not currently logged in.' ),
+            array( 'status' => 401 )
+        );
+    }
+
+    // Our custom authentication check should have no effect
+    // on logged-in requests
+    return $result;
+});
+
+/*
+add_filter( 'rest_authentication_errors', function( $result ) {
+    // If a previous authentication check was applied,
+    // pass that result along without modification.
+    if ( true === $result || is_wp_error( $result ) ) {
+        return $result;
+    }
+
 
     // Would be better to generate these routes dynamically, rather than hardcoded,
     // but I wasn't able to make the rest_get_route_for_post_type_items() work. But
@@ -52,4 +75,4 @@ add_filter( 'rest_authentication_errors', function( $result ) {
     return $result;
 });
 
-
+*/
--- public/ui-shortcodes.php
+++ public/ui-shortcodes.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-
+ // #########################################################################################################################
 
 add_shortcode( 'thet_get_applications', 'thet_get_applications');
 
@@ -41,17 +41,21 @@ function thet_get_applications(){
 
             foreach ($applications as $application) {
                 ?>
-                <div style="border: 1px solid black; margin-bottom: 0.5rem; display: flex; flex-direction: row;">
-                    <div style="width: 70%;">
-                        <h4><?php echo $application->post_title; ?></h4>
-                        <span><?php echo get_the_author_meta( 'display_name', $application->post_author ); ?></span>
-                    </div>
-                    <div style="width: 30%; display: flex; flex-direction: column; justify-content: center; padding-left: 0.5rem; padding-right: 0.5rem;;">
-                        <a href="<?php echo get_permalink( get_option( 'thet_options' )['interactive_form_page_id'] ) . '?application_id=' . strval( $application->ID ); ?>">
-                            <div style="padding: 0.8rem 1.5rem; border: 1px solid black; text-align: center;">Edit application</div>
-                        </a>
-                    </div>
+
+                <div class="wp-block-columns are-vertically-aligned-center is-layout-flex wp-container-10 wp-block-columns-is-layout-flex" style="border-width:1px;border-radius:1rem;margin-bottom:1rem;padding-top:0.5rem;padding-right:0.5rem;padding-bottom:0.5rem;padding-left:0.5rem">
+                <div class="wp-block-column is-vertically-aligned-center is-layout-flow wp-block-column-is-layout-flow" style="padding-top:1rem;padding-right:1rem;padding-bottom:1rem;padding-left:1rem;flex-basis:66.66%">
+                <h4 class="wp-block-heading"><?php echo $application->post_title ?></h4>
+                <p><?php echo get_the_author_meta( 'display_name', $application->post_author ) ?></p>
                 </div>
+                <div class="wp-block-column is-vertically-aligned-center is-layout-flow wp-block-column-is-layout-flow" style="flex-basis:33.33%">
+                <div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-8 wp-block-buttons-is-layout-flex">
+                <div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
+                <div class="wp-block-button is-style-fill"><a href="<?php echo get_permalink( get_option('thet_options')['interactive_form_page_id'] ) . '?application_id=' . $application->ID  ?>" class="wp-block-button__link has-background-color has-pale-cyan-blue-background-color has-text-color has-background wp-element-button" style="border-radius:0.25rem;padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">Edit</a></div>
+                </div>
+                </div>
+                </div>
+                </div>
+
                 <?php
             }
 
@@ -66,26 +70,26 @@ function thet_get_applications(){
 
 }
 
+
+// -------------------------------------------------------------------------------------------------------------------
+
+add_shortcode('thet_get_wheel', 'thet_get_wheel');
+
+function thet_get_wheel(){
+
+    $wheel_content = file_get_contents( plugin_dir_path(__FILE__) . 'media/new-circle-core.svg' );
+    return $wheel_content;
+
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+
 add_shortcode( 'thet_testing', 'thet_testing' );
 
 function thet_testing(){
 
-    $attr = array(
-
-        'post_type' => 'applications',
-     
-    );
-
-    $applications = get_posts( $attr );
-
-    ob_start();
-    ?>
-    <script>let thetTesting = <?php echo json_encode( $applications ); ?></script>
-    <?php
-    $output = ob_get_contents();
-    ob_end_clean();
-    return $output;
+    return "";
 
 }
 
-
+// -------------------------------------------------------------------------------------------------------------------
