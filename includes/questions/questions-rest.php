584dd26b3185238ed6a91a20d251ce7e4ae897a0
VrubPetGit <petr@vrubel.online>
2024-04-05 15:31:04 +0200

    questions to rest progress

---
+++ includes/questions/questions-rest.php
@@ -0,0 +1,20 @@
+<?php
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+/*
+add_action('rest_api_init', 'bat_rest_questions');
+
+function bat_rest_questions(){
+
+    register_rest_route('bat/api', '/questions/', [
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => bat_rest_get
+   ]);
+
+} 
+*/
